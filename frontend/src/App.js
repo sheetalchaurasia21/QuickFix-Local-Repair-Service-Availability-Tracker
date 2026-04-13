be78da4ee427b575ae8b3d3eadcd0103b1f9d52b
Aradhya-K <aradhyak1513@gmail.com>
2026-04-14 00:54:01 +0530

    created register page UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,12 @@
-function App() {
-  return <h1>QuickFix App</h1>;
-}
+// import Login from "./pages/Login.js";
+// function App() {
+//   return <Login />;
+// }
+// export default App;
 
+import Register from "./pages/Register.js";
+
+function App() {
+  return <Register />;
+}
 export default App;
