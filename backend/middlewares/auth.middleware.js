9ec4dc8f08aec8347797a871bcbbcffaf5610caa
Sheetal Chaurasia <csheetal344@gmail.com>
2026-04-25 15:29:34 +0530

    resolved merge conflict

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 function auth(req,res,next){
     const token=req.cookies.token
     if(!token){
-        res.statur(401).send({message:"Not Logged In"});
+        res.status(401).send({message:"Not Logged In"});
     }
     try{
         let decode=jwt.verify(token, process.env.JWT_SECRET);
